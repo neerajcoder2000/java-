91d8e9abdc226985a08201fbca92369c7b2bbf1b
neerajcoder2000 <91449595+neerajcoder2000@users.noreply.github.com>
2021-09-27 12:10:24 +0530

    Add files via upload

---
+++ Ingeritance/PowerClaculator.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class PowerClaculator {
+	
+	public void square(int a) {
+		
+		int sq = a*a;
+		System.out.println("Square: "+sq);
+		}
+
+	public void cube(int a) {
+		
+		int c = a*a*a;
+		System.out.println("cube: "+c);
+	}
+}
