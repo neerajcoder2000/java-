42277b858cb8d95466fae82db63ad8658af1e9d9
neerajcoder2000 <91449595+neerajcoder2000@users.noreply.github.com>
2021-09-27 12:21:46 +0530

    Add files via upload

---
+++ JDK10/VarDemo.java
@@ -0,0 +1,21 @@
+package JDK10;
+
+public class VarDemo {
+
+	public static void main(String[] args) {
+		
+		var a=10;
+		var b="Map";
+		var c='a';
+		var d=10.5;
+		var e=true;
+		
+		for(var i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+		
+		
+
+	}
+
+}
