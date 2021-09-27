4eb4ddc503aa7050b7a3573e6e817eaf74974e80
neerajcoder2000 <91449595+neerajcoder2000@users.noreply.github.com>
2021-09-27 11:48:26 +0530

    collection

---
+++ CollectionFrameWork/Collections.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		
+		//ArrayList<E>
+		//Set<E>
+		//TreeSet<E>
+
+	}
+
+}
